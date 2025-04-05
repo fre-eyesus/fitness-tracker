7ec4a913e3ec04823b23cd075b3e4e0da5cbff72
Fireweld Tesfaye <freyesus12@gmail.com>
2025-04-05 13:06:54 +0300

    Add CalorieChart component with initial layout and styling

---
+++ fitness-tracker/src/components/CalorieChart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CalorieChart() {
+  return (
+    <div className="w-[300px] md:w-[66%] h-[191px]  mx-6 md:mx-16 p-6 bg-[#EEEEEE] border border-gray-200 rounded-lg shadow-sm">
+    
+
+    </div>
+  )
+}
+
+export default CalorieChart
