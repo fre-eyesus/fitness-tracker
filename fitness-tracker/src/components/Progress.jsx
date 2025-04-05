4d6f6adec872346cc53cbfe55f9dfc715c804d0d
Fireweld Tesfaye <freyesus12@gmail.com>
2025-04-05 09:22:54 +0300

    Refactor Progress component layout and styling for improved structure and visual appeal

--- fitness-tracker/src/components/Progress.jsx
+++ fitness-tracker/src/components/Progress.jsx
@@ -2,24 +2,26 @@ import React from 'react';
 
 const Progress = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-black-100">
-      <div className="w-64 h-40 bg-gray-200 rounded-lg shadow-md p-4">
-        <div className="h-full flex flex-col justify-between">
-          <div className="text-sm font-medium text-gray-700">
-            Progress
+    <div className="mx-4 block w-[330px] h-[428px] p-6 bg-[#EEEEEE] border border-gray-200 rounded-lg shadow-sm md:w-[29.66%] md:h-[18.82%] md:mt-4 md:mx-0 md:ml-4">
+        <p className='font-bold tracking-tight text-gray-900'>Progress</p>
+        <div className='w-[200px] mt-50 mx-auto'>
+          <div className='flex flex-row justify-between mt-2'>
+            <div className='flex flex-row'><div className="mt-1 w-2 h-2 rounded-full bg-[#FFD02A]"></div><p className='text-sm px-2 font-normal text-[12px] text-gray-500'>Cardio</p></div><p className='text-xs text-medium text-[#9D9D9D]'>30 hrs</p>
           </div>
-          <div className="text-xs text-gray-500">
-            This is a credit card sized component 2
+          <div className='flex flex-row justify-between mt-2'>
+            <div className='flex flex-row'><div className="mt-1 w-2 h-2 rounded-full bg-[#FFB578]"></div><p className='text-sm px-2 font-normal text-[12px] text-gray-500'>Stretching</p></div><p className='text-xs text-medium text-[#9D9D9D]'>40 hrs</p>
           </div>
-          <div className="flex justify-end items-center">
-            <span className="text-xs bg-gray-300 px-2 py-1 rounded">
-              Details
-            </span>
+          <div className='flex flex-row justify-between mt-2'>
+            <div className='flex flex-row'><div className="mt-1 w-2 h-2 rounded-full bg-[#8676]"></div><p className='text-sm px-2 font-normal text-[12px] text-gray-500'>Treadmil</p></div><p className='text-xs text-medium text-[#9D9D9D]'>30 hrs</p>
           </div>
+          <div className='flex flex-row justify-between mt-2'>
+            <div className='flex flex-row'><div className="mt-1 w-2 h-2 rounded-full bg-[#8676FE]"></div><p className='text-sm px-2 font-normal text-[12px] text-gray-500'>Strength</p></div><p className='text-xs text-medium text-[#9D9D9D]'>20 hrs</p>
+          </div>
+          
+          
         </div>
-      </div>
     </div>
   );
 };
 
-export default Progress;
+export default Progress; 
