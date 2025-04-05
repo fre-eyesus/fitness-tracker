47ee8499ac33510f03bea0ebe86bdb269366cd94
Fireweld Tesfaye <freyesus12@gmail.com>
2025-04-05 09:24:27 +0300

    Refactor App component layout to improve structure and organization

--- fitness-tracker/src/App.jsx
+++ fitness-tracker/src/App.jsx
@@ -1,16 +1,25 @@
 import './App.css'
 import Dashboard from "./components/dashboard.jsx";
-import Activity from './components/Activity.jsx';
 import Progress from './components/Progress.jsx';
+import Activity from './components/Activity.jsx';
+import Trainer from './components/TrainerProfile.jsx';
+import DietMenu from './components/DIetMenu.jsx';
+import Footer from './components/Footer.jsx';
 function App() {
 
   return (
     <>
     <div >
       <Dashboard />
-      <Progress />
+      <div className='md:flex md:flex-row md:ml-22'>
+        <Activity />
+        <Progress />
+      </div>
+         <Trainer />
+        <DietMenu />
+      
     </div>
-   
+    <Footer />
     </>
   )
 }
