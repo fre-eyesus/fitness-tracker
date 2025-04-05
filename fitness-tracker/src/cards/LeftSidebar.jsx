76bedc71974fc5c8391d998ac09a74004b349626
Fireweld Tesfaye <freyesus12@gmail.com>
2025-04-05 13:04:35 +0300

    Add LeftSidebar component for navigation and branding

---
+++ fitness-tracker/src/cards/LeftSidebar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Home, Plus } from 'lucide-react';
+import logo from '../assets/logo.svg';
+const LeftSidebar = () => {
+  return (
+    <div className="fixed top-0 left-0 h-full w-[20%] sm:w-[15%] lg:w-[10%] xl:w-[5%] bg-[#EEEEEE] shadow-lg flex flex-col items-center p-4 space-y-10 max-lg:hidden">
+      <div className="text-xl font-bold text-indigo-600">
+        <a href='#'><img src={logo} alt="a logo for fitness tracking dashbaord" /></a>
+      </div>
+
+      <nav className="flex flex-col items-center space-y-6 text-gray-700 text-xs">
+        <a href="#" className="flex flex-col items-center hover:text-indigo-500 transition">
+          <Home size={20} />
+          <span className="mt-1 hidden lg:block">Home</span>
+        </a>
+
+        <a href="#" className="flex flex-col items-center hover:text-indigo-500 transition">
+          <Plus size={20} />
+          <span className="mt-1 hidden lg:block text-center">Calorie</span>
+        </a>
+      </nav>
+    </div>
+  );
+};
+
+export default LeftSidebar;
