aa34aa50831760625d7b6c8ec38662db07322cec
Fireweld Tesfaye <freyesus12@gmail.com>
2025-04-05 20:01:29 +0300

    Update Activity component to adjust layout for better responsiveness

--- fitness-tracker/src/components/Activity.jsx
+++ fitness-tracker/src/components/Activity.jsx
@@ -3,7 +3,7 @@ import Dropdown from "./DropDown";
 
 const Activity = () => {
   return (
-    <div class="flex justify-center mt-4 mb-4 w-[300px] h-[191px] mx-6 md:w-[29.66%] md:h-[30%] md:ml-[6.4%] ">
+    <div class="flex justify-center mt-4 mb-4 w-[300px] h-[191px] mx-6 md:w-[29.66%] md:h-[30%] md:ml-[6.4%] lg:h-full">
       <div className="w-[300px] h-[191px] pt-3 px-4 bg-gray-100 border border-gray-200 rounded-lg shadow-sm">
           <div className="flex items-center justify-between mb-4">
             <p className="font-bold tracking-tight text-gray-900">Activity</p>
