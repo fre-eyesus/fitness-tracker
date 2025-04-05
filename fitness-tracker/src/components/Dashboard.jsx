43763f43180bb7933547ff52e90da961671cd908
Fireweld Tesfaye <freyesus12@gmail.com>
2025-04-05 19:57:50 +0300

    Refactor Dashboard layout to include LeftSidebar and Sidebar components for improved structure

--- fitness-tracker/src/components/Dashboard.jsx
+++ fitness-tracker/src/components/Dashboard.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
-import Workout from '../cards/workout';
-import Menu from './Menu';
-
+import LeftSidebar from '../cards/LeftSidebar';
+import Workout from '../cards/Workout';
+import CalorieChart from './CalorieChart';
+import Trainer from './TrainerProfile';
+import DietMenu from './DIetMenu';
+import Footer from './Footer';
+import Sidebar from '../cards/sidebar';
 const Dashboard = () => {
   return (
     <>
-    <Workout />
+        <div className='lg:flex lg:flex-row '>
+        <div className='lg:w-[2%]'>
+            <LeftSidebar />
+        </div>
+        <div className='lg:w-[85%] lg:flex lg:flex-col lg:justify-end lg:gap-2' >
+         <div className=''> <Workout /></div>
+          <div className='mt-4 md:flex md:flex-row md:ml-1 md:mt-5 lg:flex lg:flex-row lg:gap-4 lg:ml-20'>
+            <CalorieChart />
+          </div>
+            <div> <Trainer /></div>
+            <div><DietMenu /></div>
+        </div>
+  
+          <div className='lg:w-[20%]'>
+            <Sidebar />
+          </div>
+    </div>
+          <Footer />
     </>
   );
 }
