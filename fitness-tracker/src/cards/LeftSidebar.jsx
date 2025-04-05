324622b48df0f01bd2def4a570d3eaaccf68cd4c
Fireweld Tesfaye <freyesus12@gmail.com>
2025-04-05 19:57:35 +0300

    Add modal functionality to LeftSidebar for calorie input

--- fitness-tracker/src/cards/LeftSidebar.jsx
+++ fitness-tracker/src/cards/LeftSidebar.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import { Home, Plus } from 'lucide-react';
 import logo from '../assets/logo.svg';
+import { useNavigate } from 'react-router-dom';
+import Modal from '../components/Modal';
+import {useState} from 'react';
+import CalorieInput from '../components/CalorieInput';
 const LeftSidebar = () => {
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => setIsModalOpen(true); 
+    const closeModal = () => setIsModalOpen(false); 
+
+
   return (
     <div className="fixed top-0 left-0 h-full w-[20%] sm:w-[15%] lg:w-[10%] xl:w-[5%] bg-[#EEEEEE] shadow-lg flex flex-col items-center p-4 space-y-10 max-lg:hidden">
       <div className="text-xl font-bold text-indigo-600">
@@ -14,11 +25,15 @@ const LeftSidebar = () => {
           <span className="mt-1 hidden lg:block">Home</span>
         </a>
 
-        <a href="#" className="flex flex-col items-center hover:text-indigo-500 transition">
+        <a href="#" className="flex flex-col items-center hover:text-indigo-500 transition" onClick={openModal}>
           <Plus size={20} />
           <span className="mt-1 hidden lg:block text-center">Calorie</span>
         </a>
       </nav>
+
+      <Modal isOpen={isModalOpen} closeModal={closeModal}>
+        <CalorieInput />
+      </Modal>
     </div>
   );
 };
