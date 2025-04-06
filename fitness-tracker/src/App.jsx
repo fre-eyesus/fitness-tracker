78002811c9a96cf7713ee697bbc1d506affe2dda
Fireweld Tesfaye <freyesus12@gmail.com>
2025-04-06 21:58:21 +0300

    Remove unused component imports from App.jsx for cleaner code

--- fitness-tracker/src/App.jsx
+++ fitness-tracker/src/App.jsx
@@ -1,14 +1,5 @@
 import './App.css'
 import Dashboard from "./components/dashboard.jsx";
-import Progress from './components/Progress.jsx';
-import Activity from './components/Activity.jsx';
-import Trainer from './components/TrainerProfile.jsx';
-import DietMenu from './components/DIetMenu.jsx';
-import Footer from './components/Footer.jsx';
-import Sidebar from './cards/sidebar.jsx';
-import Workout from './cards/Workout.jsx';
-import LeftSidebar from './cards/LeftSidebar.jsx';
-import CalorieChart from './components/CalorieChart.jsx';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CalorieInput from './components/CalorieInput.jsx';
 
