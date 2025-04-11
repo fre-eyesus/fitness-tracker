f26be1e5d0fcb3768f6f9bd42ba3f73cc426883d
Fireweld Tesfaye <freyesus12@gmail.com>
2025-04-11 23:30:38 +0300

    Import useState and useEffect hooks in Dashboard component

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import LeftSidebar from '../layout/LeftSidebar';
 import Workout from '../layout/Workout';
 import CalorieChart from './CalorieChart';
